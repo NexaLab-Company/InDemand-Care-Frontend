b24920b958837bcd485ed6550860387f5aa903da
Aneeque-Junaid <aneequej2002@gmail.com>
2023-03-11 22:03:50 +0500

    XD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test/Test';
-
-
-
-
+import LatestIncome from './components/Latest Income/LatestIncome';
 
 function App() {
-
-
   return (
-
     <div className="App">
-
-      <Test/>
-
+      <LatestIncome />
     </div>
-
-
   );
-
-};
+}
 
 export default App;
